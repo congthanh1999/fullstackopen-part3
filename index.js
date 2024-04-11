37954ef53ef45b1e859970a2d329e8f1c696124c
congthanh1999 <congthanh10021999@gmail.com>
2024-04-11 14:53:52 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const generateId = () => {
+  return Math.floor(Math.random() * 10000);
+};
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: "The name or number is missing" });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return res.status(400).json({ error: "Name must be unique" });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
+});
+
+app.get("/info", (req, res) => {
+  const current = new Date();
+  const content = `<div>Phonebook has info for ${persons.length} people</div>
+                    <br/>
+                    <div>${current}</div>`;
+  res.send(content);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
